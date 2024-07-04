be5fcac2ba858119e6e47e65146d018512b4fdee
Jose Flores Hernandez <jose.flores@elektra.com.mx>
2024-07-04 11:22:35 -0600

    project creation

---
+++ Criptoioskit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Criptoioskit
+//
+//  Created by Jose Flores Hernandez on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
